69e215ad8a292f170b5ec57edf27013f60d272ba
Matteo Porro <matteozzoporro427@gmail.com>
2023-05-24 11:38:09 +0200

    Inizio grafica (in classe assieme)

--- js/index.js
+++ js/index.js
@@ -20,3 +20,41 @@ var carosello = [
         desc:"...."
     }
 ];
+
+let esercizio= {
+    id:0,
+    //testo esercizio/domanda
+    argomento:" ",
+
+    difficoltà: 0,
+    consegna: " ",
+
+    /**
+     * -risposta multipla
+     * -testo bucato
+     * -vero/falso
+     */
+    //in base al tipo interpreto l'oggetto risposte in modo opportuno
+    tipo: "",
+
+    risposte:{
+        punteggio:0,
+    }
+
+}
+
+let profiloUtente={
+    idUtente: 0,
+    classe:"",
+    nome:"",
+    cognome:"",
+    email:"",
+    livello:{},//indicare il livello di conoscenza/esercizio/abilità
+    preferenze:{},
+    storicoEsercizi:{}
+}
+
+let documento={
+
+}
+
