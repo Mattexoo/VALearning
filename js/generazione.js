39f964fc3c8232fa7eb7de96ed78a244cd6e9e0c
Matteo Porro <matteozzoporro427@gmail.com>
2023-05-31 10:14:02 +0200

    qualche mod

--- js/generazione.js
+++ js/generazione.js
@@ -2,4 +2,35 @@
  * FILE FINALIZZATO ALLA GENERAZIONE CASUALE DI VERIFICHE(IN BASE AI FILTRI SELEZIONATI DALL'UTENTE)
  */
 
-let select = document.getElementById("")
+
+// Variabili globali
+
+let livello = "A2"; // variabile contenente il livello dell'utente(gestito in precedenza)
+
+//in alternativa, si può usare profiloUtente.livello ma è un oggetto più complesso che verrà gestito da altri gruppi, quindi usiamo 
+//per semplicità questa variabile di esempio
+
+// Seleziona livello di inglese in base a quello selezionato
+function selezionaLivello(){
+    let options = document.getElementsByTagName("option");
+    let selezionato = false
+    let i = -1
+    while(!selezionato){
+        if(options[++i].value==livello){
+            options[i].selected = true;
+            selezionato = true;
+        }
+        else 
+            options[i].selected = false;
+
+    }
+
+}
+
+let nEs = document.getElementById("nEsercizi");
+
+nEs.value = 3;
+
+function genera(){
+
+}
